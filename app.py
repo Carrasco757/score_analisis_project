ffb15fb3f213a6ad998d208b3e83f3c3d3f4f28b
Carrasco757 <45760589+Carrasco757@users.noreply.github.com>
2020-11-11 10:12:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
+import streamlit as st
+
 #precisa escrever o texto? Beleza
-st.write('hola!')
-#quer mostrar a tabela? Não tem problema
-st.write(df)
-#aguenta gráficos? Tranquilo
-st.write(plotlyfigure)
-#e muito mais...
+st.write('Ola Mundo!')
